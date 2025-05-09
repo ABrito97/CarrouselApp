bb59a6472c3db3a68376c7baa58e170831f4468a
ABrito97 <brito9539@gmail.com>
2025-05-09 14:05:18 -0600

    Initial commit

---
+++ CarrouselApp/CarrouselAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrouselAppApp.swift
+//  CarrouselApp
+//
+//  Created by Alejandro Brito on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CarrouselAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
